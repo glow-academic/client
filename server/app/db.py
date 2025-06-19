05861c19e45ea9681b9cb75def593ed6d214258a
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-06-18 22:14:09 -0500

    docker

--- server/app/db.py
+++ server/app/db.py
@@ -21,6 +21,13 @@ engine = create_engine(db_url)
 
 
 def init_db() -> None:
+    # Skip schema creation if running in Docker environment
+    # Docker initialization already creates the schema from SQL files
+    if os.getenv("DOCKER_ENV"):
+        print("🐳 Running in Docker - skipping SQLModel schema creation (using SQL files instead)")
+        return
+    
+    print("🔧 Creating database schema via SQLModel...")
     SQLModel.metadata.create_all(engine)
 
 
