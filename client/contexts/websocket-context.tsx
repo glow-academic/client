b4edfb10ec57ff2e199d36be5462670acfc5a626
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-10-15 19:52:51 -0400

    fixes

--- client/contexts/websocket-context.tsx
+++ client/contexts/websocket-context.tsx
@@ -11,7 +11,7 @@ import {
   attemptsFullKeys,
   layoutContextKeys,
 } from "@/lib/api/v2/keys";
-import { log, type LogEntry } from "@/lib/api/v2/server/logs";
+import { log, type LogEntry } from "@/utils/logger";
 import { useQueryClient } from "@tanstack/react-query";
 import React, {
   createContext,
