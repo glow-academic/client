4407d7a3d84ab3343217cb4b87cef107bd6dc1ab
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-11-19 12:55:54 -0500

    fixes

---
+++ client/app/login/page.tsx
@@ -0,0 +1,17 @@
+/**
+ * app/login/page.tsx
+ * This is the login page.
+ * @AshokSaravanan222 & @siladiea
+ * 05/14/2025
+ */
+import type { Metadata } from "next";
+import Login from "@/components/auth/Login";
+
+export const metadata: Metadata = {
+  title: "Login | GLOW",
+  description: `Login to GLOW (Graduate Learning Orientation Workshop) at ${process.env["NEXT_PUBLIC_CAMPUS"]}`,
+};
+
+export default function LoginPage() {
+  return <Login />;
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,17 @@
 /**
  * app/page.tsx
- * This is the login page.
+ * This is the homepage.
  * @AshokSaravanan222 & @siladiea
  * 05/14/2025
  */
 import type { Metadata } from "next";
-import Login from "@/components/auth/Login";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: "Login | GLOW",
-  description: `Login to GLOW (Graduate Learning Orientation Workshop) at ${process.env["NEXT_PUBLIC_CAMPUS"]}`,
+  title: "Home | GLOW",
+  description: `GLOW (Graduate Learning Orientation Workshop) at ${process.env["NEXT_PUBLIC_CAMPUS"]}`,
 };
 
-export default function LoginPage() {
-  return <Login />;
+export default function HomePage() {
+  return redirect("/login");
 }
