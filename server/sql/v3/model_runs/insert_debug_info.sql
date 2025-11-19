0e5d416926a3f7c24df867eed03b6830b1f14707
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-11-19 14:26:07 -0500

    fixes

--- server/sql/v3/model_runs/insert_debug_info.sql
+++ server/sql/v3/model_runs/insert_debug_info.sql
@@ -1,5 +1,5 @@
 -- Insert debug info for a model run
 -- Parameters: $1=model_run_id (uuid), $2=content (text)
-INSERT INTO model_run_debug_info (model_run_id, content, created_at)
+INSERT INTO debug_info (model_run_id, content, created_at)
 VALUES ($1::uuid, $2::text, NOW())
 
