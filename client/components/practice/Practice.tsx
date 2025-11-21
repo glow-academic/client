711588834e7debe53ce3f57038371e4f74e21cc4
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-11-21 15:59:39 -0500

    fixes

--- client/components/practice/Practice.tsx
+++ client/components/practice/Practice.tsx
@@ -304,7 +304,7 @@ export default function Practice({
               | "guest",
           }}
           onStartSimulation={handleStartSimulation}
-          onStartInfiniteMode={isGuest ? undefined : handleStartInfiniteMode}
+          onStartInfiniteMode={isGuest ? (() => {}) : handleStartInfiniteMode}
           loadingSimulation={loadingSimulation}
         />
       </div>
