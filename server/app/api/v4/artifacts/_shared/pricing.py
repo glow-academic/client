df1e59f5610f210d79369419bb49d4580aa86b42
Ashok Saravanan <ashko.saravanan@gmail.com>
2026-02-14 11:29:14 -0500

    Update pricing.py

--- server/app/api/v4/artifacts/_shared/pricing.py
+++ server/app/api/v4/artifacts/_shared/pricing.py
@@ -14,9 +14,6 @@ from app.utils.cache.cache_key import cache_key
 from app.utils.cache.get_cached import get_cached
 from app.utils.cache.set_cached import set_cached
 
-SQL_PATH_PRICING = "app/sql/v4/queries/resources/pricing/get_pricing_batch_complete.sql"
-SQL_PATH_UNITS = "app/sql/v4/queries/resources/units/get_units_batch_complete.sql"
-
 
 async def _batch_fetch_pricing(
     conn: asyncpg.Connection,
