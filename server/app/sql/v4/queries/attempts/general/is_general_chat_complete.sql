b0970905e11ddfde31de46e1ee8c54f8e2db1ef7
Ashok Saravanan <ashko.saravanan@gmail.com>
2026-02-14 16:14:38 -0500

    Delete is_general_chat_complete.sql

--- server/app/sql/v4/queries/attempts/general/is_general_chat_complete.sql
+++
@@ -1,9 +0,0 @@
--- Check if chat_id is general (simple query, not function)
--- $1 = chat_id
-SELECT EXISTS (
-    SELECT 1
-    FROM view_simulation_chats_entry c
-    JOIN view_simulation_attempts_entry a ON a.id = c.attempt_id
-    WHERE c.id = $1
-      AND a.practice IS FALSE
-) as is_general;
