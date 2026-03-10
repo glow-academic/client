747c549d649cd6ad64c892e4c8188161caaa057e
Ashok Saravanan <ashko.saravanan@gmail.com>
2026-03-10 15:31:21 -0400

    fixes

--- server/app/infra/provider/draft.py
+++ server/app/infra/provider/draft.py
@@ -128,6 +128,7 @@ async def patch_provider_draft_impl(
                 group_id=profile.group_id,
                 session_id=session_id,
                 version=new_version,
+                profile_ids=[profile.profiles_id],
                 name_ids=[request.name_id] if request.name_id else None,
                 description_ids=[request.description_id]
                 if request.description_id
